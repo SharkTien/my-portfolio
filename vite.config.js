307c3b82b9c72301dba23a799c2bd11261710004
Tien Huynh <mineshark15@gmail.com>
2024-12-03 15:16:00 +0700

    gic

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-//https://github.com/ErickKS/vite-deploy
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
