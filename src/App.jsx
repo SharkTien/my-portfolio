02ef0d76c3fe891e89367fae54262951775e27f6
Tien Huynh <mineshark15@gmail.com>
2024-12-03 16:59:40 +0700

    check

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import Header from './components/header/header';
 import Home from './components/home/home';
 import About from './components/about/about';
-import Skills from './components/skills/skills';
+import Skills from './components/skills/Skills';
 import Services from './components/services/services';
 import Qualification from './components/qualification/qualification';
 import Footer from './components/footer/footer';
